f1ca95b7f0541f7444c9c7f026860227548b69a4
Marash_79@mail.ru <MarinaShulkova>
2022-05-03 22:30:24 +0300

    add class

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+
+
+    public static int menu_print() {
+
+        System.out.println(
+                "1 Enter student \n" +
+                        "2 Sort students in ascending order \n" +
+                        "3 Sort students in descending order \n" +
+                        "4 Delete student by index \n" +
+                        "5 Exit");
+
+        System.out.println("Choose menu item: ");
+        Scanner scr = new Scanner(System.in);
+        int menu_item = scr.nextInt();
+    }
 }
---
+++ .idea/src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    String name;
+    String surname;
+    int age;
+    int class_school;
+
+
+}
