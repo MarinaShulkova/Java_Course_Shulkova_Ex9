e427c6b7062879841a413bfded74d85bdf454030
samidster <samidster@gmail.com>
2022-05-16 22:17:40 +0300

    samid

--- src/main.java
+++ src/main.java
@@ -17,7 +17,7 @@ public class main {
                 "2 Sort students in ascending order \n" +
                 "3 Sort students in descending order \n" +
                 "4 Delete student by index \n" +
-                "5 Exit");
+                "5 Exit ");
 
         System.out.println("Choose menu item: ");
         Scanner scr = new Scanner(System.in);
